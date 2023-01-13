2c4d6c99242732ba2b61753c433a4e4ed4eb6b22
Edouard Desgrée <edouard.desgree@gmail.com>
2023-01-13 15:19:43 +0100

    css updates on form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,34 +1,19 @@
 export default function Form() {
   return (
     <form>
-      <div className="field-group">
-        <div className="field">
-          <label className="label">First line</label>
-          <div className="control">
-            <input
-              className="input"
-              type="text"
-              name="firstLine"
-              placeholder="Text input"
-            />
-          </div>
-        </div>
-
-        <div className="field">
-          <label className="label">Second line</label>
-          <div className="control">
-            <input
-              className="input"
-              type="text"
-              name="secondLine"
-              placeholder="Text input"
-            />
-          </div>
-        </div>
-      </div>
-      <div className="field">
-        <button type="submit">Get a new meme image</button>
-      </div>
+      <input
+        className="input"
+        type="text"
+        name="firstLine"
+        placeholder="First text"
+      />
+      <input
+        className="input"
+        type="text"
+        name="secondLine"
+        placeholder="Second text"
+      />
+      <button type="submit">Get a new meme image</button>
     </form>
   );
 }
