065aee40bfcb3f4814408e46caaa679bd64fae5a
Edouard Desgrée <edouard.desgree@gmail.com>
2023-08-22 02:18:25 +0200

    react logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import Logo from '../assets/logo.png';
+import LogoReact from '../assets/react.svg';
 
 export default function Header() {
   return (
     <header>
       <img src={Logo} className="logo" alt="logo" />
-      My Meme Generator
+      My Meme Generator in <img src={LogoReact} className="" alt="react" />
     </header>
   );
 }
